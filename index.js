d0c42b0ea2bc06e526b60cee611d4b6e8b852d04
Anthony Mugendi <ngurumugz@gmail.com>
2022-10-27 04:33:36 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,124 @@
+/**
+ * Copyright (c) 2022 Anthony Mugendi
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+const path = require('path');
+const fs = require('fs');
+const LiveDirectory = require('live-directory');
+const util = require('util');
+const cons = require('consolidate');
+const pMemoize = require('p-memoize');
+
+
+
+function is_object(value) {
+	if (!value) return false;
+	return value.toString() === '[object Object]';
+}
+
+function middleware(opts = {}) {
+	if (!is_object(opts)) {
+		throw new Error('Options passed to middleware must be an object');
+	}
+
+	opts = Object.assign(
+		{ engine: 'pug', dir: 'views', ttl: 1000 * 3600 },
+		opts
+	);
+
+	// make view path
+	// console.log(module.parent.path);
+
+	const viewsDir = path.isAbsolute(opts.dir)
+		? opts.dir
+		: path.resolve(module.parent.path, opts.dir);
+
+	// ensure directory exists
+	if (!fs.existsSync(viewsDir))
+		throw new Error(`The views directory ${opts.dir} does not exist!`);
+
+	// determine engine function
+	if (!cons[opts.engine]) {
+		throw new Error(
+			`The engine ${opts.engine} is not supported. See https://github.com/tj/consolidate.js#supported-template-engines`
+		);
+	}
+
+	const templateEngine = cons[opts.engine];
+
+	// setup the other important methods
+
+	function template_render(file, data = {}) {
+		if (!is_object(data)) {
+			throw new Error('Arg #2 passed to render() must be an object');
+		}
+		// console.log('Rendering', file.path);
+
+		return templateEngine(file.path, data).catch((error) => {
+			throw error;
+		});
+	}
+
+	const memoizedTemplateRender = pMemoize(template_render, {
+		maxAge: opts.ttl,
+		cacheKey: (args) => {
+			// make key using the file content and data passed
+			// This way we memoize the file for as long as:
+			// 1. the contents stay the same
+			// 2. the data used to render it stays the same
+			let key = args[0].content + ' > ' + util.inspect(args[1]);
+			// console.log({key});
+			return key;
+		},
+	});
+
+	// Create LiveDirectory instance
+	const liveTemplates = new LiveDirectory({
+		path: viewsDir,
+	});
+
+	// Handle 'reload' event from LiveDirectory so we can add the renderFile function
+	liveTemplates.on('file_reload', (file) => {
+		// bind the memoized render function to ensure we only
+		file.renderFile = memoizedTemplateRender.bind(null, file);
+	});
+
+	return (request, response, next) => {
+		// add the render function to response
+        // this is the function we use within our routes
+		response.render = async function (filePath, data = {}) {
+			// ensure leading slash
+			if (/^\//.test(filePath) === false) {
+				filePath = '/' + filePath;
+			}
+
+			// try and get file
+			const template = liveTemplates.get(filePath);
+
+			if (template) {
+				// render or throw any errors
+				await template
+					.renderFile(data)
+                    // post content out
+					.then((html) => {
+						response.status(200).html(html).end();
+					})
+                    // throw all errors
+					.catch((error) => {
+						throw error;
+					});
+			} else {
+				throw new Error(
+					`Template ${filePath} does not exist in ${viewsDir}`
+				);
+			}
+		};
+
+		next();
+	};
+}
+
+module.exports = middleware;
---
+++ readme.md
@@ -0,0 +1,55 @@
+<!--
+ Copyright (c) 2022 Anthony Mugendi
+
+ This software is released under the MIT License.
+ https://opensource.org/licenses/MIT
+-->
+
+# live-directory-views
+
+Before diving in, you should read about [LiveDirectory](https://github.com/kartikk221/live-directory) by [Kartik](https://github.com/kartikk221) to see why you really want to use it!
+
+This module acts as a middleware to help render all your templates. To do so:
+
+-   [live-directory](https://www.npmjs.com/package/live-directory) watches the views directory for any file changes.
+-   [p-memoize](https://www.npmjs.com/package/p-memoize) is used to memoize rendering for 1 hour by default (see ttl option below) unless the template changes and/or the data used to render it changes.
+-   [consolidate](https://www.npmjs.com/package/consolidate) helps with the multi-engine rendering.
+
+Well tested with [hyper-express](https://www.npmjs.com/package/hyper-express) but should in theory work with [express](https://www.npmjs.com/package/express) too. Let me know if it doesn't.
+
+## How to use
+
+```javascript
+const HyperExpress = require('hyper-express');
+const webserver = new HyperExpress.Server();
+const liveDirectoryViews = require('live-directory-views');
+
+// options you can add to your views middleware
+// example below includes all the default values
+let viewOptions = {
+    // your rendering engine. See https://github.com/tj/consolidate.js#supported-template-engines
+	engine: 'pug',
+    // the directory where your template files are 
+	dir: 'views',
+    // how long should we cache template renders where the template or data hasn't changed
+	ttl: 1000 * 3600,
+};
+
+// set it before any routes
+webserver.use(liveDirectoryViews(viewOptions));
+
+// Render the template using .render() method
+webserver.get('/', (request, response) => {
+
+    // this method is added the the middleware
+    // so dont go looking for it from hyper-express
+	response.render('user.pug', {name:"Anthony Mugendi"})
+
+});
+
+// Activate webserver by calling .listen(port, callback);
+webserver
+	.listen(80)
+	.then((socket) => console.log('Webserver started on port 80'))
+	.catch((error) => console.log('Failed to start webserver on port 80'));
+```
